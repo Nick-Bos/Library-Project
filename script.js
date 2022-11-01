712e3cdb3d5a214aabf24b15f9b71496d4b84c28
Nicholas Bos <90960995+Nick-Bos@users.noreply.github.com>
2022-11-01 19:01:55 +1030

    feat: :zap: Add onclick funtion to add book button to toggle form for filling out new book entry

--- script.js
+++ script.js
@@ -1,9 +1,23 @@
 let myLibrary = [];
 
-function Book(author, title, numberOfPages, isRead) {
+const addBookBtn = document.getElementById("add-book-btn");
+addBookBtn.onclick = function () {
+  popUpFormToggle(".pop-up-form", "pop-up-form--on");
+};
+
+function popUpFormToggle(classname, classnameOpen) {
+  const formDiv = document.querySelector(classname);
+  if (formDiv.classList.contains(classnameOpen)) {
+    formDiv.classList.remove(classnameOpen);
+  } else {
+    formDiv.classList.add(classnameOpen);
+  }
+}
+
+function Book(author, title, pages, isRead) {
   this.author = author;
   this.title = title;
-  this.numberOfPages = numberOfPages;
+  this.pages = pages;
   this.isRead = isRead;
 }
 
@@ -15,3 +29,10 @@ let newBook = new Book(
 );
 
 function addBookToLibrary() {}
+
+// when called, this function displays the books in the myLibrary array.
+function displayBooks() {
+  for (i = 0; i < myLibrary.length; i++) {
+    console.log(myLibrary[i]);
+  }
+}
