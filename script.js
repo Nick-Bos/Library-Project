12fa198183b56d408fc5e3150b1540e912f335e6
Nicholas Bos <90960995+Nick-Bos@users.noreply.github.com>
2022-11-02 17:57:23 +1030

    docs: :zap: Add checkbox toggle function, add functions to send form data to variables for form input storage

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 let myLibrary = [];
 
+let author = "unknown";
+let title = "unknown";
+let pages = "unknown";
+let read = "unknown";
+
 const addBookBtn = document.getElementById("add-book-btn");
 addBookBtn.onclick = function () {
   popUpFormToggle(".pop-up-form", "pop-up-form--on");
@@ -13,6 +18,47 @@ function popUpFormToggle(classname, classnameOpen) {
     formDiv.classList.add(classnameOpen);
   }
 }
+
+//form checkbox on off function
+const checkbox = document.getElementById("read it?");
+checkbox.onclick = () => {
+  checkbox.value = "true";
+  checkboxToggle();
+};
+function checkboxToggle() {
+  if (checkbox.value == "Not read") {
+    read = "Not read";
+  } else {
+    checkbox.value = "I have read this";
+  }
+}
+
+//form input data
+function sendFormData() {
+  let authorInput = document.getElementById("author").value;
+  let titleInput = document.getElementById("title").value;
+  let pagesInput = document.getElementById("pages").value;
+  let readInput = document.getElementById("read it?").value;
+
+  author = authorInput;
+  title = titleInput;
+  pages = pagesInput;
+  read = readInput;
+}
+
+const formSubmitBtn = document.getElementById("submit-btn");
+formSubmitBtn.onclick = () => {
+  submitForm();
+};
+function submitForm() {
+  sendFormData();
+  console.log(author);
+  console.log(title);
+  console.log(pages);
+  console.log(read);
+}
+
+function createBookCard() {}
 
 function Book(author, title, pages, isRead) {
   this.author = author;
